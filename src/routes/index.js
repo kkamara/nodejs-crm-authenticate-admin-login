1bcae0370420e11843ad23317bd3dc125840b8d0
kkamara <kamaracomputers@gmail.com>
2023-07-09 23:26:53 +0100

    Add auth api routes for www.github.com/kkamara/nodejs-crm-desktop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const home = require('./home');
+const api = require('./api');
 
 const router = express.Router();
 
 router.use('/', home);
+router.use('/', api);
 
 module.exports = router;
