6850b9542b02edef0e2aee40d98a49cd8abc9d8c
kkamara <kamaracomputers@gmail.com>
2023-07-19 23:02:18 +0100

    Log form data to console

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('form.login-form');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  console.log(event);
+  console.log('username', event.target.elements.username.value);
 });
