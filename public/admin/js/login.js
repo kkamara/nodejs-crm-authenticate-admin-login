320e824f287d394ccdc5da1883085eda4c575797
kkamara <kamaracomputers@gmail.com>
2023-07-19 23:38:05 +0100

    Log admin login request

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,5 +1,11 @@
 const form = document.querySelector('form.login-form');
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', async function(event) {
   event.preventDefault();
-  console.log('username', event.target.elements.username.value);
+  const username = event.target.elements.username.value;
+  const password = event.target.elements.password.value;
+  console.log('username', username);
+  const res = await axios.post(domain+'/admin', {
+    username, password,
+  });
+  console.log(res.data);
 });
